f71e7d3692aae98156a28d511ac2d024f9a31925
lancelot <lancelot.b@outlook.com>
2017-05-14 13:35:26 +0800

    140517#01: add a few three.js animations

---
+++ vcent.js
@@ -0,0 +1,46 @@
+"user strict";
+
+define([ "/lib/jquery" ], function ($) {
+	var vcent = {};
+
+	vcent.update = function () {
+		var e = $(".vcenter");
+		var i;
+		var cent = function (pr, e) { e.css("top", ((pr.innerHeight() - e.outerHeight()) / 2) + "px"); };
+
+		e.css("position", "relative");
+
+		for (i = 0; i < e.length; i++) {
+			cent($(e[i]).parent(), $(e[i]));
+		}
+
+		cent = function (pr, e) { e.css("top", ((pr.innerHeight() - e.outerHeight()) / 2) + "px"); };
+
+		e = $(".avcenter");
+		e.css("position", "absolute");
+
+		for (i = 0; i < e.length; i++) {
+			cent($(e[i]).parent(), $(e[i]));
+		}
+
+		return;
+	};
+
+	vcent.reset = function (com) {
+		if (com.hasClass("vcenter") ||
+			com.hasClass("avcenter")) {
+			com.removeClass("vcenter");
+			com.removeClass("avcenter");
+			com.css("top", "0");
+		}
+	};
+
+	vcent.update();
+	$(window)
+		.on("load", vcent.update)
+		.on("resize", vcent.update);
+
+	setInterval(vcent.update, 50);
+
+	return vcent;
+});
